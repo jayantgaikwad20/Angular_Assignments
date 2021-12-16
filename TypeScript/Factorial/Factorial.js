f77ab0999f877936bae3950d4717a75b63f39041
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-12-16 20:06:37 +0530

    Add files via upload

---
+++ TypeScript/Factorial/Factorial.js
@@ -0,0 +1,20 @@
+// function factorial(num:number)
+// {
+//     var fact=1;
+//     for(let i=1;i<=num;i++)
+//     {
+//        fact=fact*i;
+//     }
+//      return fact;
+// }
+// console.log(factorial(10));
+function factorial(num) {
+    var fact = 1;
+    var i = 1;
+    while (i <= num) {
+        fact = fact * 1;
+        i++;
+    }
+    return fact;
+}
+console.log(factorial(3));
