0b917831c137754aac9fe4b3fa9b9cc02b758003
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-12-16 20:08:10 +0530

    Add files via upload

---
+++ TypeScript/Function/FunctionWithReturnType.ts
@@ -0,0 +1,20 @@
+function sum(num1,num2)
+{
+    return(num1+num2);
+}
+var Add=sum(10,20);
+console.log(Add);
+
+function substract(num3,num4)
+{
+    return(num3+num4);
+}
+var Substract=substract(5,2)
+   console.log(Substract);
+
+   function multiplication(num3,num4)
+{
+    return(num3+num4);
+}
+var Mult=multiplication(5,2)
+   console.log(Mult);
