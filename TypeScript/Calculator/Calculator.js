09c5f1c80d57472124ea641bccdb6120ad235a35
Jayant  Gaikwad <92709730+jayantgaikwad20@users.noreply.github.com>
2021-12-16 20:02:38 +0530

    Add files via upload

---
+++ TypeScript/Calculator/Calculator.js
@@ -0,0 +1,21 @@
+function Calculate(num1, num2, op) {
+    if (op.charAt(0) == '+') {
+        return num1 + num2;
+    }
+    else if (op.charAt(0) == '-') {
+        return num1 - num2;
+    }
+    else if (op.charAt(0) == '*') {
+        return num1 * num2;
+    }
+    else if (op.charAt(0) == '/') {
+        return num1 / num2;
+    }
+    else {
+        return "Invalid";
+    }
+}
+console.log(Calculate(30, 10, '-'));
+console.log(Calculate(30, 10, '*'));
+console.log(Calculate(30, 10, '/'));
+console.log(Calculate(30, 10, '+'));
